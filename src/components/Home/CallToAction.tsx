885e70c2268c8286924ba960b789f3cf80869b3f
Damhafi <yusufmilla@gmail.com>
2023-06-18 14:13:06 -0300

    feature: mobile section CallToAction

--- src/components/Home/CallToAction.tsx
+++ src/components/Home/CallToAction.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FaLongArrowAltDown } from 'react-icons/fa';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import ArrowDown from '@public/arrowDown.svg';
 import Icon from '@public/icon.svg';
 
 import farm from '@public/farm.png';
@@ -22,9 +24,9 @@ export const CallToAction: React.FC = () => {
       <Image className="absolute top-0 -z-10" src={farm} alt="background" />
       <Icon className="absolute right-0 top-20 -z-10 h-1/2 w-1/2 opacity-[0.04]" />
 
-      <div className="container flex">
-        <div className="relative -mb-8 -mr-16 ml-10 h-1/4 flex-1 self-end">
-          <div className="absolute -top-6 left-32 h-44 w-[1px] bg-green">
+      <div className="container flex flex-col-reverse gap-9 md:flex-row lg:gap-2">
+        <div className="relative -mb-8 -mr-16 hidden h-1/4 flex-1 self-center md:flex lg:ml-10 lg:self-end">
+          <div className="absolute -top-6 left-20 h-44 w-[1px] bg-green lg:left-32">
             <div className="relative flex">
               <div className="absolute right-6 h-2 w-2 -translate-y-1/2 rounded bg-green" />
               <p className="absolute ml-4 w-24 -translate-y-1/2 text-xs font-semibold text-white">
@@ -32,7 +34,8 @@ export const CallToAction: React.FC = () => {
               </p>
             </div>
           </div>
-          <div className="absolute left-64 h-44 w-[1px] bg-white">
+
+          <div className="absolute left-48 h-40 w-[1px] bg-white lg:left-64 lg:h-44">
             <div className="relative flex">
               <div className="absolute right-6 h-2 w-2 -translate-y-1/2 rounded bg-white" />
               <p className="absolute ml-4 w-24 -translate-y-1/2 text-xs font-semibold text-white">
@@ -43,15 +46,24 @@ export const CallToAction: React.FC = () => {
           <Image src={tablet} alt="tablet" priority />
         </div>
 
-        <div className="mb-20 mt-24 w-1/2 text-white">
-          <div className="flex w-3/5 flex-col gap-8">
-            <h1 className="whitespace-pre text-5xl font-bold leading-[1.10]">
+        <div className="mb-20 mt-24 w-full text-white md:w-1/2">
+          <div className="flex w-full flex-col gap-8 sm:w-3/5">
+            <h1 className="whitespace-pre text-4xl font-bold leading-[1.10] xs:text-5xl">
               Gestão {'\n'}
               inteligente {'\n'}
               de processos {'\n'}
               agrícolas
             </h1>
-            <h2 className="order-first flex items-center gap-3">
+            <div className="relative flex w-full md:hidden">
+              <ArrowDown />
+              <Image
+                src={tablet}
+                alt="tablet"
+                priority
+                className="absolute -right-[40%] -top-[35%]"
+              />
+            </div>
+            <h2 className="order-first flex flex-col gap-3 xs:items-center sm:flex-row">
               <div className="mt-3 h-[2px] w-4 bg-green font-normal" />
               Nós te damos informações importa...
             </h2>
@@ -62,9 +74,10 @@ export const CallToAction: React.FC = () => {
             </p>
             <Link
               href="/"
-              className="w-fit rounded border border-green p-5 text-sm font-semibold"
+              className="flex w-full items-center justify-center gap-4 rounded border border-green p-5 text-center text-sm font-semibold xs:w-fit"
             >
               Conheça nossa solução
+              <FaLongArrowAltDown color="#fffff" />
             </Link>
           </div>
         </div>
